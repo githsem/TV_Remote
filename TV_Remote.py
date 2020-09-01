f6f74dadc7408445be3a18609d31939251f0cf0e
githsem <53379599+githsem@users.noreply.github.com>
2020-09-02 00:26:40 +0200

    Update TV_Remote.py

--- TV_Remote.py
+++ TV_Remote.py
@@ -15,4 +15,25 @@ class Kumanda():
                 if (self.tv_ses !=0):
                     self.tv_ses -= 1
                     print("Ses : ",self.tv_ses)
-                    
+            elif (karakter == ">"):
+                if (self.tv_ses !=30):
+                    self.tv_ses += 1
+                    print("Ses : ",self.tv_ses)
+            else:
+                print("Ses guncellendi...")
+                break
+
+    def tv_kapat(self):
+        print("Tv Kapatiliyor")
+        self.tv_durum = "Kapali"
+
+    def tv_ac(self):
+        print("Tv Aciliyor")
+        self.tv_durum = "Acik"
+
+    def __str__(self):
+        return "TV Durumu : {}\nTV Ses : {}\nKanallar : {}\nSuanki Kanal : {}".format(self.tv_durum,self.tv_ses,self.kanal_listesi,self.kanal
+
+    def __len__(self):
+        print()
+
